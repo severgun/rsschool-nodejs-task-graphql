532361d5f3bc58c00b1a3063b12d534ffc661f78
Sergey Vergun <sewergun@gmail.com>
2024-03-03 19:12:17 +0300

    feat: add user.profile and user.posts fields resolvers

--- src/routes/graphql/gql-schemas/users/usersObj.ts
+++ src/routes/graphql/gql-schemas/users/usersObj.ts
@@ -2,6 +2,7 @@ import { GraphQLFloat, GraphQLList, GraphQLObjectType, GraphQLString } from "gra
 import { UUIDType } from "../../types/uuid.js";
 import { PostObj } from "../posts/postsObj.js";
 import { ProfileObj } from "../profiles/profileObj.js";
+import { PrismaClient, User } from "@prisma/client";
 
 export const UserObj = new GraphQLObjectType({
   name: 'User',
@@ -9,8 +10,26 @@ export const UserObj = new GraphQLObjectType({
     id: { type: UUIDType },
     name: { type: GraphQLString },
     balance: { type: GraphQLFloat },
-    profile: { type: ProfileObj },
-    posts: { type: new GraphQLList(PostObj)},
+    profile: { 
+      type: ProfileObj,
+      resolve: async (source: User, _, context: PrismaClient) => {
+        return await context.profile.findUnique({
+          where: {
+            userId: source.id,
+          }
+        })
+      }
+    },
+    posts: { 
+      type: new GraphQLList(PostObj),
+      resolve: async (source: User, _, context: PrismaClient) => {
+        return await context.post.findMany({
+          where: {
+            authorId: source.id,
+          }
+        })
+      }
+    },
     // userSubscribedTo: {},
     // subscribedToUser: {},
   })
