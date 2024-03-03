a343293a093693897813f752b8c7997e6c766be5
Sergey Vergun <sewergun@gmail.com>
2024-03-03 19:00:56 +0300

    fix: change user profile field type

--- src/routes/graphql/gql-schemas/users/usersObj.ts
+++ src/routes/graphql/gql-schemas/users/usersObj.ts
@@ -1,6 +1,7 @@
 import { GraphQLFloat, GraphQLList, GraphQLObjectType, GraphQLString } from "graphql";
 import { UUIDType } from "../../types/uuid.js";
 import { PostObj } from "../posts/postsObj.js";
+import { ProfileObj } from "../profiles/profileObj.js";
 
 export const UserObj = new GraphQLObjectType({
   name: 'User',
@@ -8,7 +9,7 @@ export const UserObj = new GraphQLObjectType({
     id: { type: UUIDType },
     name: { type: GraphQLString },
     balance: { type: GraphQLFloat },
-    profile: { type: UUIDType },
+    profile: { type: ProfileObj },
     posts: { type: new GraphQLList(PostObj)},
     // userSubscribedTo: {},
     // subscribedToUser: {},
